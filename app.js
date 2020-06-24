8020ee096150e950cf5fdae2bc654077fda413ed
fashion.sheikh <hammadn788@gmai.com>
2020-06-24 11:56:23 -0700

    javascript

---
+++ app.js
@@ -0,0 +1,515 @@
+// ___________________chapter #01  task no# 1____________________
+
+// alert("Welcome to Our Page")
+
+// ___________________chapter #01  task no# 2____________________
+// alert( "Error!Please enter the Correct Password. \n \n Prevent this message from additional dialoges ") 
+
+//     // console.log()
+// alert("Welcome to JS land")
+
+// ___________________chapter #01  task no# 3____________________
+//     alert("Welcome to JS land..\n\n Happy Coding!")
+
+    
+// ___________________chapter #01  task no# 4____________________
+
+    // var a = "Hello..! I can run JS through web console"
+    // console.log(a)
+
+    // ___________________chapter #01  task no# 5____________________
+
+    // var a =  prompt("usernsme");
+    // document.write (a)
+
+    // ___________________chapter #02  task no# 1____________________
+
+//     var message = "Hello world" ;
+//     alert(message)
+
+// ___________________chapter #02  task no# 2____________________
+
+// var    email =   "Email Address"  ; 
+// alert( "My  "+email + " is hammadn788@gmail.com")
+
+// ___________________chapter #02  task no# 3____________________
+
+// var book = "Smarter way to Learn Javascript"
+// alert( "I am Try to learn from the book" + book )
+
+// ___________________chapter #02  task no# 4____________________
+
+// var a = "Yah! I can Write HTML code in JAVASCRIPT"
+// document.write(a)
+
+// ___________________chapter #02  task no# 5____________________
+
+// var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(a)
+
+// ___________________chapter #03  task no# 1____________________
+
+// var age = "I am 15 Years Old"
+// alert(age)
+
+// ___________________chapter #03  task no# 2____________________
+
+// var page =  "“You have visited this site 20 times”."
+
+// document.write(page)
+
+// ___________________chapter #03  task no# 3____________________
+
+// var birthyear =   2001 ;
+
+// document.write("My Birth Year is" +  " " +  birthyear)
+
+// // ___________________chapter #03  task no# 4____________________
+// var Enter_your_Name = "Enter your Name";
+// prompt("Enter_your_Name");
+
+// var Enter_your_product_title = "Enter your product title";
+// prompt("Enter_your_product_title");
+ 
+// var Enter_your_Quantity = " Enter your Quantity"
+// prompt("Enter_your_Quantity")
+
+// document.write( Enter_your_Name + " " + "ordered" + Enter_your_product_title +  "Tshirt(s)" + "" + "On XYZ clothing" )
+
+
+// ___________________chapter #04  task no# 1____________________
+
+// var a = "hammad" + "Anus" + "Hamza"
+// document.write(a)
+
+// ___________________chapter #04  task no# 2____________________
+
+// var a  =  "<h1>Rules for naming JS variables</h1>"
+// document.write(a)
+// var b = "<p>Variable names can only contain number, $ and _ .For example $my_1stVariable </p>"
+// document.write(b)
+// var c = "<p>Variables must begin with a Letter, $ or_ . For example $name, _name or name </p>"
+// document.write(c)
+
+// var d = "<p>Variables names are case sensitive </p>"
+// document.write(d)
+
+// var e = "<p>Variables names should not be JS keywords </p>"
+// document.write(e)
+
+// ___________________chapter 05  task no# 1____________________
+// var a =  prompt("enter the first number ")
+// var b =  prompt ("enter the 2nd number ")
+
+// var c = (a + b) ;
+// document.write(c)
+ 
+
+// ___________________chapter #05  task no#02 ____________________
+
+// var a =  prompt("enter the first number ")
+// var b =  prompt ("enter the 2nd number ")
+
+// var c = (a * b) ;
+// document.write(c)
+ 
+// var a =  prompt("enter the first number ")
+// var b =  prompt ("enter the 2nd number ")
+
+// var c = (a % b) ;
+// document.write(c)
+ 
+// var a =  prompt("enter the first number ")
+// var b =  prompt ("enter the 2nd number ")
+
+// var c = (a - b) ;
+// document.write(c)
+ 
+// var a =  prompt("enter the first number ")
+// var b =  prompt ("enter the 2nd number ")
+
+// var c = (a / b) ;
+// document.write(c)
+ 
+
+// ___________________chapter #05  task no#03 ____________________
+
+// var a = "Value after variable declaration is: ”"
+// var b = "Initial value: 5"
+
+// for (var b =a;b <=a;b++ ){
+//     document.write(b)
+// }
+
+
+// ___________________chapter #05  task no#04 ____________________
+
+// var a = "<h1>Total cost of buy 5 tickets to a moive is 3000PKR</h1>"
+
+// document.write(a)
+
+
+
+// ___________________chapter #05  task no#05 ____________________
+// var a ="<center><h1>TABLE OF 2</h1></center>"
+// document.write(a)
+
+
+//  document.write("2" + "x" + "1" + "=" + 2*1 + "<br>")
+//  document.write("2" + "x" + "2" + "=" + 2*2 + "<br>")
+//  document.write("2" + "x" + "3" + "=" + 2*3 + "<br>")
+//  document.write("2" + "x" + "4" + "=" + 2*4 + "<br>")
+//  document.write("2" + "x" + "5" + "=" + 2*5 + "<br>")
+//  document.write("2" + "x" + "6" + "=" + 2*6 + "<br>")
+//  document.write("2" + "x" + "7" + "=" + 2*7 + "<br>")
+//  document.write("2" + "x" + "8" + "=" + 2*8 + "<br>")
+//  document.write("2" + "x" + "9" + "=" + 2*9 + "<br>")
+//  document.write("2" + "x" + "10" + "=" + 2*10 + "<br>")
+
+
+// ___________________chapter #05  task no#05 ____________________
+// var a = "<center><h1>THE LIFETIME SUPPLY CALCULATOR </h1></center>"
+// document.write(a)
+// var b = "<p>My Favroite snack : RIO</p>"
+// document.write(b)
+// var c = "<p>current age : 17</p> "
+// document.write(c)
+// var d = "<p>estimated maximum age: 70</p> "
+// document.write(d)
+// var e = "<p> Amount of snacks per day : 3</p>"
+// document.write(e)
+// var f = "<p>WIll you need 50 RIO biscuits untill the ripie gold age of 65</p> "
+// document.write(f)
+
+
+// ___________________chapter #06  task no#01 ____________________
+// var a = "<p> Result:</p>"
+// document.write(a)
+// var b = "<p> The value of a is:10</p>"
+// document.write(b)
+// var c = "<p> ------------</p>"
+// document.write(c)
+// var d = "<p> The value of a++ is:11</p>"
+// document.write(d)
+// var e = "<p> Now the value of a is :11</p>"
+// document.write(a)
+// var f = "<p> -----------</p>"
+// document.write(f)
+// var g = "<p> The value of a++ is:11</p>"
+// document.write(g)
+
+// var h = "<p> Now the value of a is :12</p>"
+// document.write(h)
+
+// var i = "<p> The value of --a is:11</p>"
+// document.write(i)
+
+// var j = "<p> Now the value of a is :11</p>"
+// document.write(j)
+
+// var k = "<p> The value of a-- is :11</p>"
+// document.write(k)
+// var k = "<p> now value of a is :10</p>"
+// document.write(k)
+
+
+// ___________________chapter #06  task no#02 ____________________
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+
+// for (var result =0 ; result<=2 ; result++){
+//     for(var result =0 ; result<=1 ; result++){
+//         document.write("a--" +  "<br>" + "b--" )
+//     }
+//     document.write(b--)
+// }
+
+// ___________________chapter #06  task no#03 ____________________
+// var a = prompt("Enter your name :")
+
+// document.write(a)
+
+// ___________________chapter #0  task no#04 ____________________
+// var a = prompt("Enter the 1st subject")
+// var b = prompt("Enter the 2nd subject")
+// var c = prompt("Enter the 3rd subject")
+
+
+// var total_subject =  100;
+
+
+// ___________________chapter #09  task no#01 ____________________
+
+//  var a = prompt(" Enter your city name")
+
+//  if ( a ===karachi){
+//      document.write("Welcome to city of light")
+//      }else("city is not valid")
+
+// ___________________chapter #09  task no#02 ____________________
+// var  gender = prompt("Enter your gander")
+
+// if(gender === male){
+//     alert( "welcome sir.!")
+// }else if(gender === female){
+//     alert("welcome ma'am")
+// }
+
+
+// ___________________chapter #09  task no#03 ____________________
+// var go ="a. var a = 4 <P>if (++a === 5){ </p> alert(given condition for variable a is true);}<p>;"
+
+// document.write (go)
+
+
+// ___________________chapter #09  task no#04 ____________________
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+//  var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//  var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+
+
+
+// ___________________chapter #09  task no#04 ____________________
+// var a =  prompt("Enter the number  :")
+// var b = "Number is divisibale"
+// var c = "Number is not divisable"
+// if( a == 3){
+//     alert(b)
+// }else{
+//     alert(c)
+// }
+
+
+// ___________________chapter #09  task no#04 ____________________
+// var a = prompt("Enter the tempreture")
+// var T1 = "It is too hot outside"
+// var T2 = "The Weather today is Normal"
+// var T3 = "Today’s Weather is cool"
+// var T4 = "OMG! Today’s weather is so Cool."
+// if(a > 40){
+//     alert(T1)
+// }else if(a >30){
+//     alert(T2)
+// }else if(a >20) {
+//     alert(T3)
+// }
+// else if(a >10){
+//     alert(T4)
+// }
+
+
+
+// ___________________chapter #09  task no#04 ____________________
+
+
+
+// var ch =	 prompt("Enter any character:");
+	
+
+	// if (ch>=65 && ch<=90)
+	// 	cout<<"Character is a capital letter";
+	// else if (ch>=97 && ch<=122)
+	// 	cout<<"Character is a small letter";
+	// else if (ch>=48 && ch<=57)
+	// 	cout<<"Character is a digit";
+	// else if ((ch>0 && ch<=47)||(ch>=58 && ch<=64)||
+    //            (ch>=91 && ch<=96)||(ch>=123 && ch<=127))
+	// 	cout<<"Character is a special symbol";
+	
+	// return 0;
+
+
+
+
+// ___________________chapter #09  task no#04 ____________________
+
+
+
+// var  greeting = prompt("Enter the number");
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";}
+// else{
+// greeting = ("Good evening");
+// }
+
+
+
+// ___________________chapter #09  task no#04 ____________________
+// var n = prompt("Enter the radius")
+// var pi = 3.142;
+// var area = 3.142 * n*n;
+
+// document.write(area + "  is the area of circle")
+
+
+// ___________________chapter #09  task no#04 ____________________
+// var a =  prompt("Enter the  number")
+
+// document.write(a)
+
+
+// ___________________chapter #16  task no#04 ____________________
+
+// var arr = ["Hammad"+ " "  +"Anus"+ " " +"Shahid"]
+// document.write(arr)
+
+
+// ___________________chapter #16  task no#04 ____________________
+
+// var anyBoxesChecked = [];
+// var numeroPerguntas = 5;     
+// for(var i=0;i<numeroPerguntas;i++)
+// {
+//    anyBoxesChecked.push(false);
+// }
+
+
+// ___________________chapter #16  task no#05 ____________________
+
+
+// var arr = ["Hammad", "Nadeem"]
+// var lastNmae = ["sheikh", "Akther"]
+// for(var i = 0;i< arr.length ;i++){
+// 	alert(arr[i] + " " + lastNmae[j])
+// }
+
+
+// var students = [['David', 80], ['Vinoth', 77], ['Divya', 88], ['Ishitha', 95], ['Thomas', 68]];
+
+// var Avgmarks = 0;
+
+// for (var i=0; i < students.length; i++) {
+//         Avgmarks += students[i][1];
+//         var avg = (Avgmarks/students.length);
+// }
+
+// ___________________chapter #16  task no#05 ____________________
+// var students = [['David', 80], ['Vinoth', 77], ['Divya', 88], ['Ishitha', 95], ['Thomas', 68]];
+
+// var Avgmarks = prompt("enter the number")
+
+// for (var i=0; i < students.length; i++) {
+//         Avgmarks += students[i][1];
+//         var avg = (Avgmarks/students.length);
+// }
+
+// document.write("Average grade: " + (Avgmarks)/students.length);
+
+//         if (avg < 60){
+//           document.write("Grade : F");      
+//           } 
+//         else if (avg < 70) {
+//                 document.write("Grade : D"); 
+//                   } 
+//         else if (avg < 80) 
+//              {
+//                 document.write("Grade : C"); 
+//         } else if (avg < 90) {
+// 			document.write("Grade : B"); 
+//         } else if (avg < 100) {
+// 			document.write("Grade : A"); 
+// }
+
+
+// ___________________chapter #16  task no#05 ____________________
+
+// var arr = ["0"+"1"+ "2"+"3" + "<br>"+ "1" + "0" +"1"+ "2" +"<br>"+ "2"+"1" +"0"+ "1" +"<br>"]
+// document.write (arr)
+
+
+// ___________________chapter #16  task no#05 ____________________
+
+// for(var i=1 ; i <= 10 ;i++){
+// 	document.write(i + "<br>")
+// }
+
+
+// ___________________chapter #16  task no#05 ____________________
+
+// var a = prompt("Enter the any table Number")
+
+//  document.write( a + "x" + "1" + "=" + a*1 + "<br>")
+//  document.write( a + "x" + "2" + "=" + a*2 + "<br>")
+//  document.write( a + "x" + "3" + "=" + a*3 + "<br>")
+//  document.write( a + "x" + "4" + "=" + a*4 + "<br>")
+//  document.write( a  + "x" + "5" + "=" + a*5 + "<br>")
+//  document.write( a + "x" + "6" + "=" + a*6 + "<br>")
+//  document.write( a + "x" + "7" + "=" + a*7 + "<br>")
+//  document.write( a + "x" + "8" + "=" + a*8 + "<br>")
+//  document.write( a + "x" + "9" + "=" + a*9 + "<br>")
+//  document.write( a + "x" + "10" + "=" + a*10 + "<br>")
+
+
+
+// ___________________chapter #16  task no#05 ____________________
+
+// var fruits = ["Apple" + "<br> " +   "Banana"+ "<br> " + "Mango"+ " <br>" + "Orange" + "<br> " + "Strawberry"]
+// document.write(fruits)
+
+
+// ___________________chapter #16  task no#05 ____________________
+
+// var arr1 = ["counting :" + "1","2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" ,"13", "14", "15" + "<br>"]
+
+// document.write(arr1)
+
+// var arr2 = [ "Reverse counting:"+ "10", "9", "8", "7", "6", "5", "4", "3", "2", "1", "<br>"]
+
+// document.write(arr2)
+
+// var arr3 = [ "Even:" + " 0", "2", "4"," 6"," 8", "10", "12", "14", "16"," 18", "20" + "<br>"]
+
+// document.write(arr3)
+
+// var arr4 = [  "Odd :" + "1", "3", "5", "7"," 9", "11", "13", "15", "17", "19" + "<br>"]
+
+// document.write(arr4)
+
+// var arr4 = [ "Series:" + " 2k","4k", "6k"," 8k", "10k", "12k", "14k", "16k", "18k", "20k" + "<br>"]
+
+// document.write(arr4)
+
+
+// ___________________chapter #16  task no#05 ____________________
+var arr = prompt("Enter the Your Product")
+var arr1 =  ["“cake”", "“apple pie”", "“cookie”", "“chips”", "“patties”"]
+
+if (arr == arr1){
+	document.write(arr1 + "is availbe in our store")
+}
+
+
+
+
